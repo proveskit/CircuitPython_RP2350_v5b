74880d00d006da54aa6f5f7906c36d942db437c6
ineskhou <127782958+ineskhou@users.noreply.github.com>
2025-10-01 19:01:20 -0700

    added hashlib and flight validation (#15)
    * added hashlib and flight validation
    
    * correct import

--- src/flight-software/repl.py
+++ src/flight-software/repl.py
@@ -12,6 +12,7 @@ from lib.adafruit_tca9548a import TCA9548A  # This is Hacky V5a Devel Stuff###
 from lib.pysquared.beacon import Beacon
 from lib.pysquared.cdh import CommandDataHandler
 from lib.pysquared.config.config import Config
+from lib.pysquared.file_validation.manager.file_validation import FileValidationManager
 from lib.pysquared.hardware.burnwire.manager.burnwire import BurnwireManager
 from lib.pysquared.hardware.busio import _spi_init, initialize_i2c_bus
 from lib.pysquared.hardware.digitalio import initialize_pin
@@ -202,6 +203,8 @@ def all_faces_on():
     load_switch_3.enable_load()
     load_switch_4.enable_load()
 
+
+file_validator = FileValidationManager(logger)
 
 ## Face Sensor Stuff ##
 
